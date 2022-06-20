4f255d9a6245af1b1c2e31635e5058233f097958
Isaac-Franklin <praisefranklin005@gmail.com>
2022-06-20 16:38:03 +0100

    Zuri JS Assignment

---
+++ main.js
@@ -0,0 +1,8 @@
+var NameSlot = document.querySelector(".myname");
+NameSlot.innerHTML = "My name is Isaac Tochukwu Franklin";
+
+var MyHeight = document.querySelector(".myheight");
+MyHeight.innerHTML = "I am 1.7m tall";
+
+var MyCountry = document.querySelector(".mycountry");
+MyCountry.innerHTML = "I am a Nigerian";
